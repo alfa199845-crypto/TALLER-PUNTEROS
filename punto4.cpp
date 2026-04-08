9be1cc3613b25934694864af7d2581f9a799d986
alfa199845-crypto <alfa199845@gmail.com>
2026-04-08 16:45:33 -0500

    Update punto4.cpp

--- punto4.cpp
+++ punto4.cpp
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int duplicar(int x) {
     return x * 2;
@@ -9,21 +10,46 @@ int triplicar(int x) {
 }
 
 void aplicar(int *arr, int n, int (*func)(int)) {
+
     for (int i = 0; i < n; i++) {
         *(arr + i) = func(*(arr + i));
     }
 }
 
 int main() {
-    int arr[] = {1, 2, 3, 4};
-    int n = 4;
 
-    aplicar(arr, n, duplicar);
+    int n;
 
-    printf("Resultado:\n");
+    cout << "Ingrese cantidad de numeros: ";
+    cin >> n;
+
+    int *arr = new int[n];
+
     for (int i = 0; i < n; i++) {
-        printf("%d ", *(arr + i));
+        cout << "Numero " << i + 1 << ": ";
+        cin >> *(arr + i);
     }
+
+    int opcion;
+
+    cout << "\n1. Duplicar\n2. Triplicar\nOpcion: ";
+    cin >> opcion;
+
+    if (opcion == 1) {
+        aplicar(arr, n, duplicar);
+    } else {
+        aplicar(arr, n, triplicar);
+    }
+
+    cout << "\nResultado:\n";
+
+    for (int i = 0; i < n; i++) {
+        cout << *(arr + i) << " ";
+    }
+
+    cout << endl;
+
+    delete[] arr;
 
     return 0;
 }
